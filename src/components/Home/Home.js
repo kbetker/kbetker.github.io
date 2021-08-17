0f3443c2dc7e0b5b82d4b7a6d0e0688dbec33ddc
kbetker <kevin@betker.org>
2021-08-17 14:24:57 -0500

    added some placeholder images to my homepage. updated some error handling on the contact form

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useRef, useState } from "react"
 import "./Home.css"
+import img0 from "./images/img0.jpg"
+import img1 from "./images/img1.jpg"
+import img2 from "./images/img2.jpg"
+import img3 from "./images/img3.jpg"
+import img4 from "./images/img4.jpg"
+import img5 from "./images/img5.jpg"
+import img6 from "./images/img6.jpg"
+import img7 from "./images/img7.jpg"
+import img8 from "./images/img8.jpg"
+import img9 from "./images/img9.jpg"
+
+
 import { titlesArray, waitAMoment, colors } from "../MiscComponents/MiscComponents.js"
 
 
 function Home() {
-  // const [title, setTitle] = useState('')
   const title = useRef('')
+  const homeImage = useRef()
+  const backGroundImage = useRef()
   const [opacity, setOpacity] = useState(1)
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9]
 
 
   let count = 0;
@@ -35,6 +49,9 @@ function Home() {
 
   const changeTitle = async () => {
     let currTitle = titlesArray[count];
+    homeImage.current.src = images[count]
+    homeImage.current.classList.remove("hidden")
+
     const titleDiv = document.getElementById("colors")
     for (let i = 0; i < currTitle.length; i++) {
       let char = currTitle[i]
@@ -49,13 +66,21 @@ function Home() {
       titleDiv.innerHTML = `${title.current}`
     }
 
+
     if (count === titlesArray.length - 1) {
       count = 0;
       setTimeout(async () => {
+        backGroundImage.current.style.backgroundImage = `url(${img0})`
+        homeImage.current.classList.add("hidden")
         clearTitle(title.current)
       }, 3000);
     } else {
       count++
+      setTimeout(() => {
+      homeImage.current.classList.add("hidden")
+      backGroundImage.current.style.backgroundImage = `url(${images[count-1]})`
+      }, 2000);
+
       setTimeout(async () => {
         changeTitle()
       }, 3000);
@@ -89,9 +114,15 @@ function Home() {
 
   return (
     <div className="page_element" style={{ width: `${window.innerWidth}px` }} id="page-1" >
-      <div className="homeText">
-        <div>Hi, my name is <span className="highlightColor">Kevin Betker</span>. I am ...</div>
-        <div><span className="highlightColor" id="colors"></span> <span style={{ opacity: `${opacity}` }}>|</span></div>
+      <div className="homeContainter">
+        <div className="homeImage" style={{backgroundImage: `url(${img0})`}} ref={backGroundImage}>
+          <img src={img2} className="image hidden" ref={homeImage}></img>
+        </div>
+
+        <div className="homeText">
+          <div>Hi, my name is <span className="highlightColor">Kevin Betker</span>. I am ...</div>
+          <div><span className="highlightColor" id="colors"></span> <span style={{ opacity: `${opacity}` }}>|</span></div>
+        </div>
       </div>
     </div>
   )
