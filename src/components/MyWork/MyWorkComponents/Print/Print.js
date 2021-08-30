bf79c95219f49579e199d0f40ac2f5fa089f537c
kbetker <kevin@betker.org>
2021-08-30 11:36:16 -0500

    added images to MyWork-Print

--- src/components/MyWork/MyWorkComponents/Print/Print.js
+++ src/components/MyWork/MyWorkComponents/Print/Print.js
@@ -2,6 +2,9 @@ import React, { useEffect, useRef } from "react"
 import { useDispatch } from "react-redux"
 import "./Print.css"
 import { myWorkFunc } from "../../../store/myWork";
+import print1 from "./images/print1.jpg"
+import print2 from "./images/print2.jpg"
+import print3 from "./images/print3.jpg"
 
 
 
@@ -29,8 +32,31 @@ function Print(){
     const dispatch = useDispatch();
     return(
         <div className="darkBackground transparent" onClick={(e)=> closePage(e)} ref={darkBackground} id="close">
-            <div className="preview previewHide" ref={preview}>
-            Print
+            <div className="printPreview previewHide" ref={preview}>
+                <div className="close_X" id="close_X">X</div>
+
+                <div className="printImages">
+                        <div className="printLeftImage">
+                            <img src={print1} className="sb_mainImg"></img>
+                            </div>
+                        <div className="printRightImages">
+                            <img src={print2} className="printTopRight"></img>
+                            <img src={print3}></img>
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <h2>Print Materials</h2>
+                        <div className="sb_text">
+                        <p>
+                            print allows users to upload audio files to create soundscapes for roleplaying games or any type of interactive story telling. print offers plenty of options to organize and customize sounds to optimize the user's experience. Users can interact with the sounds using the app's built-in buttons and sliders or even connect to an external MIDI device for precision control.
+                        </p>
+                        <a href="https://print.herokuapp.com/" className="link" target="_blank"> Click here to view the site live =&gt; SoundBlender.com</a>
+                        </div>
+                    </div>
+
+
             </div>
         </div>
     )
